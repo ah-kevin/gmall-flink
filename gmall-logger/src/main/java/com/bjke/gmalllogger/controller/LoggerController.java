48ead54f9b7d7b9b7ce9db9df186a1e148a64d22
bjke <kebingjun@zhuge.com>
2021-12-28 13:35:57 +0800

    add

--- gmall-logger/src/main/java/com/bjke/gmalllogger/controller/LoggerController.java
+++ gmall-logger/src/main/java/com/bjke/gmalllogger/controller/LoggerController.java
@@ -1,11 +1,18 @@
 package com.bjke.gmalllogger.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class LoggerController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     @RequestMapping("test")
     public String test1() {
@@ -22,8 +29,10 @@ public class LoggerController {
     public String getLog(@RequestParam("param") String jsonStr) {
         System.out.println(jsonStr);
         // 数据落盘
-
+        log.info(jsonStr);
         // 数据写入kafka
+        kafkaTemplate.send("ods_base_log", jsonStr);
+
         return "success";
     }
 }
